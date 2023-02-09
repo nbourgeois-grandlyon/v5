dac3f5091badf71c098f7a2fd30e3caa9543558b
brgnao <nbourgeois@grandlyon.com>
2023-02-09 10:46:13 +0100

    métro

---
+++ tilesets/Métropole.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Métropole" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../../../Users/brgna/Documents/escape game - Copie/tilesets/Métropole.png" width="96" height="64"/>
+</tileset>
